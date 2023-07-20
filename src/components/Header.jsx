a2bd126823845b656ef0d69c8e1cb78e7d256dbe
vaishnavimysore <mysorevaishnavi@gmail.com>
2023-07-19 19:30:21 -0500

    Shimmer UI, Search button func, useEffect hook usage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import imgLogo from "../../images/foodDeliveryLogo.jpeg";
+import { useState } from "react";
 
 //Fucntional component for Header part of our app which contains Logo, Nav items etc
 const Header = () => {
+  const [btnName, btnNameUpdate] = useState("Login");
+
   return (
     <div className="header">
       <div className="Logo-container">
@@ -13,6 +16,16 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="btn"
+            onClick={() => {
+              btnName === "Login"
+                ? btnNameUpdate("Logout")
+                : btnNameUpdate("Login");
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
