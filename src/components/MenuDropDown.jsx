24a5f5bd2b701942f6d6c61e26a49d0c6f72e5e4
vaishnavimysore <mysorevaishnavi@gmail.com>
2023-08-29 19:49:25 -0700

    Changes in styles for menu drop down

--- src/components/MenuDropDown.jsx
+++ src/components/MenuDropDown.jsx
@@ -3,18 +3,24 @@ import { ITEMS_IMG_URL } from "../utils/constants";
 
 const MenuDropDown = (props) => {
   const { menuData } = props;
-  const { name, price, imageId } = menuData?.card?.info;
+  const { name, price, imageId, description } = menuData?.card?.info;
+  console.log(menuData?.card?.info);
   return (
-    <div className="bg-white flex flex-wrap justify-between border-solid border border-l-0 border-r-0 py-4">
-      <div>
-        <h1 className="mb-2  ml-4 text-base font-normal leading-tight text-neutral-800 dark:text-neutral-50">
+    <div className="bg-white border-solid flex justify-between  border border-l-0 border-r-0 py-4 text-left">
+      <div className="">
+        <span className="mb-2  ml-4 text-base font-normal leading-tight text-neutral-800 dark:text-neutral-50">
           {name}
-        </h1>
-        <p className="mb-2  ml-4 text-sm text-neutral-600 dark:text-neutral-200">
+        </span>
+        <span className="mb-2  ml-4 text-base text-neutral-600 dark:text-neutral-200">
           - Rs {price / 100}
-        </p>
+        </span>
+        <p className="text-xs text-gray-400 w-64 ml-4 mt-2">{description}</p>
       </div>
+
       <div>
+        <button className="rounded-md bg-black absolute text-white ml-16 mt-24 p-1">
+          Add +
+        </button>
         <img
           className="rounded-md w-[125px] h-[125px] object-fill  space-x-9"
           src={ITEMS_IMG_URL + imageId}
